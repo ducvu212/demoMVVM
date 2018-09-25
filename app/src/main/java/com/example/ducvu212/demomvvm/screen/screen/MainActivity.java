fa37ecf1f969bf5c3ad2d1637d85de5820481e39
DUC_PC <minhducvu.212@gmail.com>
2018-09-25 16:10:57 +0700

    Update Base

---
+++ app/src/main/java/com/example/ducvu212/demomvvm/screen/screen/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ducvu212.demomvvm.screen.screen;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.example.ducvu212.demomvvm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/ducvu212/demomvvm/utils/Constant.java
@@ -0,0 +1,17 @@
+package com.example.ducvu212.demomvvm.utils;
+
+/**
+ * Created by le.quang.dao on 10/03/2017.
+ */
+
+public final class Constant {
+    public static final String END_POINT_URL = "https://api.github.com";
+    // For bundle
+    public static final String ARGUMENT_LIST_USER = "ARGUMENT_LIST_USER";
+    public static final String ARGUMENT_USER_ID = "ARGUMENT_USER_ID";
+    public static final String BREAK_LINE = "\n";
+
+    private Constant() {
+        // No-op
+    }
+}
